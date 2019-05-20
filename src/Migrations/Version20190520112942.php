00fbfc65119e558e9e7f10777743358d348b8588
Yavuz <kutuk.y@expoweb.fr>
2019-05-20 13:35:28 +0200

    Quete 6

--- src/Migrations/Version20190520112942.php
+++ src/Migrations/Version20190520112942.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190520112155 extends AbstractMigration
+final class Version20190520112942 extends AbstractMigration
 {
     public function getDescription() : string
     {
